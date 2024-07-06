d898a18542cf4dacf21877b721e7c64738d44109
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-07-06 18:28:55 +0300

    Add links to meals and tables page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ const Header: React.FC = () => {
     <header className="bg-orange-600 shadow">
         <div className="container mx-auto px-4 py-6 flex justify-between items-center">
             <div className="flex space-x-4">
-                <a href="#" className="text-white hover:text-gray-200">Meals</a>
-                <a href="#" className="text-white hover:text-gray-200">Tables</a>
+                <a href="/meals" className="text-white hover:text-gray-200">Meals</a>
+                <a href="/tables" className="text-white hover:text-gray-200">Tables</a>
             </div>
             <div>
                 <a href="#" className="text-white hover:text-gray-200">Log in</a>
