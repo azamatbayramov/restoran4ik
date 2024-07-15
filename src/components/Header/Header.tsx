58882e41e44e92984867d242e5365ba386469984
Angelika2707 <anzelika272011@gmail.com>
2024-07-16 00:17:32 +0300

    main page with adaptation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header: React.FC = () => {
     return (
-    <header className="bg-orange-600 shadow">
-        <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+    <header className="bg-orange-600 shadow h-16">
+        <div className="container mx-auto px-4 py-6 flex justify-between items-center h-full">
             <div className="flex space-x-4">
                 <a href="/meals" className="text-white hover:text-gray-200">Meals</a>
                 <a href="/tables" className="text-white hover:text-gray-200">Tables</a>
