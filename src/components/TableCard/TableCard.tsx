e35310da85e5e6ba5d6cf4719a8bd8ebddb96ea0
MariiaChugaeva <m.chugaeva@innopolis.university>
2024-07-20 00:48:03 +0300

    add tables page func.

--- src/components/TableCard/TableCard.tsx
+++ src/components/TableCard/TableCard.tsx
@@ -1,19 +1,21 @@
-// components/TableCard.tsx
+import React from "react";
 
 interface TableCardProps {
-    title: string;
-    isOccupied: boolean;
+  title: string;
+  isOccupied: boolean;
 }
 
 const TableCard: React.FC<TableCardProps> = ({ title, isOccupied }) => {
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-6">
-            <div className="font-bold text-xl mb-2">{title}</div>
-            <p className={`text-base ${isOccupied ? 'text-red-500' : 'text-green-500'}`}>
-                {isOccupied ? 'Occupied' : 'Available'}
-            </p>
-        </div>
-    );
+  return (
+    <div className="w-72 h-32 flex justify-center items-center bg-gray-300 rounded relative shadow-md">
+      <div className="text-2xl font-semibold">{title}</div>
+      <div
+        className={`absolute right-4 top-1/2 transform -translate-y-1/2 w-6 h-6 rounded-full ${
+          isOccupied ? "bg-yellow-300" : "bg-green-500"
+        }`}
+      ></div>
+    </div>
+  );
 };
 
 export default TableCard;
